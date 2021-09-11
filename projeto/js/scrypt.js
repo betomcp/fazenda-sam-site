c4685a5fd7b81cf32b06380c47c371539c0d49b1
betomcp <beto.porto2000@gmail.com>
2021-09-11 02:32:28 -0300

    site continuação

---
+++ projeto/js/scrypt.js
@@ -0,0 +1,15 @@
+var aux = 1
+var maxImages = 4
+
+function changeImage(){
+    let image = document.getElementById('section-1-image')
+    aux++
+    if(aux > maxImages){
+        aux = 1
+        image.style.backgroundImage = `url(../images/fundos-section-1/fundo-1-${aux}.jpg)`
+    }else{
+        image.style.backgroundImage = `url(../images/fundos-section-1/fundo-1-${aux}.jpg)`
+    }
+}
+
+window.setInterval(changeImage, 5500)
